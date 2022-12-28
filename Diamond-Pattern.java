ccf7c9b00dd7bf52b818eca269055f7abdd005b5
MirMuhammad Jabbari <44081918+M-CAP7AIN@users.noreply.github.com>
2022-12-28 08:33:26 +0330

    Update Diamond-Pattern.java

--- Diamond-Pattern.java
+++ Diamond-Pattern.java
@@ -1,39 +1,53 @@
-// with (2n+1)
-//   *
-//  ***
-// *****
-//*******
-// *****
-//  ***
-//   *
+/******************************************************************************
+		      ** Diamond pattern with (2n+1) **
+    *
+   ***
+  *****
+ *******
+  *****
+   ***
+    *
+*******************************************************************************/
 
 
-public class Main
-{
-	public static void main(String[] args) {
 
-		int input = 3;
+
+public class Main {
+	
+    public static void main(String[] args) {
+	
+	// Input value
+        int input = 3;
+
+	    
+	// Up
+        for (int i = 0; i <= input; i++) {
 		
-		for (int i =0;i<=input;i++ ) {
-		    for (int s =input-i;s> 0;s--){
-		        System.out.print(" ");
-		    }
-		    
-		    for (int j=0;j<(2*i+1);j++ ) {
-		      System.out.print("*");  
-		    }
-		    System.out.println("");
-		}
+	    // Space
+            for (int s = input - i; s > 0; s--) {
+                System.out.print(" ");
+            }
 		
-		for (int i =input-1;i>= 0;i-- ) {
-		    for (int s =input-i;s > 0;s--){
-		        System.out.print(" ");
-		    }
+	    // Star
+            for (int j = 0; j < (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+	
+	// Down
+        for (int i = input - 1; i >= 0; i--) {
 		
-		    for (int j=0;j<(2*i+1);j++ ) {
-		      System.out.print("*");  
-		    }
-		    System.out.println("");
-		}
-	}
+	    // Space
+            for (int s = input - i; s > 0; s--) {
+                System.out.print(" ");
+            }
+
+	    // Star
+            for (int j = 0; j < (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 }
